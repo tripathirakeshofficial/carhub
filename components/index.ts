07e718ba12857f2380d28825f7f2c2f6d82d7d34
Rakesh Tripathi <tripathirakeshofficial@gmail.com>
2023-07-24 23:49:57 +0530

    Car Catalogue

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Hero from "./hero";
 import CustomButton from "./custom-button";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import SearchBar from "./search-bar";
+import CustomFilter from "./custom-filter";
+import SearchManufacturer from "./search-manufacturer";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
